f633c86b05572f90bee9faac6362ce81a4524f51
jeff5m <jeffmatheuspereira@gmail.com>
2021-09-01 00:21:09 -0300

    feat: returning 404 status when no anime is found

--- src/main/java/academy/devdojo/webflux/controller/AnimeController.java
+++ src/main/java/academy/devdojo/webflux/controller/AnimeController.java
@@ -4,10 +4,12 @@ import academy.devdojo.webflux.domain.Anime;
 import academy.devdojo.webflux.service.AnimeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -29,6 +31,11 @@ public class AnimeController {
     public Mono<Anime> findById(@PathVariable int id) {
         log.info("Finding anime with id: {} in database", id);
         return animeService.findById(id)
+                .switchIfEmpty(monoResponseStatusException())
                 .log(); // not necessary
+    }
+
+    private <T> Mono<T> monoResponseStatusException() {
+        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found"));
     }
 }
